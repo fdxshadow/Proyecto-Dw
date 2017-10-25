b7a994a453d7de15533b352095e9e7623d86d276
CristianRojass <cristian.rojas@alumnos.uv.cl>
2017-10-24 23:55:08 -0300

    actualizacion

--- assets/js/egresados.js
+++ assets/js/egresados.js
@@ -13,8 +13,37 @@ $(document).ready(function() {
     }
   });
 
-
-
+  	$('i').hover(function() {
+        $(this).css('cursor','pointer');
+    });
+    $('.fa-trash-o').click(function(event){
+      swal({
+        title: '¿Estás seguro?',
+        text: "No podrás revertir ésto!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, eliminar!',
+        cancelButtonText: 'No, cancelar!',
+        confirmButtonClass: 'btn btn-success',
+        cancelButtonClass: 'btn btn-danger'
+      }).then(function () {
+        swal(
+          'Eliminado!',
+          'Se ha eliminado un egresado',
+          'success'
+        )
+      }, function (dismiss) {
+        if (dismiss === 'cancel') {
+          swal(
+            'Cancelado',
+            'No se ha eliminado',
+            'error'
+          )
+        }
+      })
+    });
 
   $.formUtils.addValidator({
     name : 'rutFormato',
@@ -28,15 +57,9 @@ $(document).ready(function() {
   $.formUtils.addValidator({
     name : 'rutDigitoVerificador',
     validatorFunction : function(value, $el, config, language, $form) {
-      var sum = 0,
-          mul = 2;
-      for(i = (value.length - 3) ; i >= 0; i--) {
-        sum = sum + value.charAt(i) * mul;
-        if(mul == 7){
-          mul = 2;
-        } else {
-          mul++;
-        }
+      var sum = 0;
+      for(i=(value.length-3),mul=0;i>=0;i--,mul++){
+        sum+=value.charAt(i)*((mul)%6+2);
       }
       digito  = 11 - (sum % 11);
       if(digito == 11){
