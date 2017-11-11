bbd025580ee6e4e053633236191880ca4696b288
ivetteleonfaune <ivette.leon@alumnos.uv.cl>
2017-11-11 21:29:06 -0200

    Crud agregar empleo y supervisor

--- api/controllers/JobController.js
+++ api/controllers/JobController.js
@@ -6,6 +6,144 @@
  */
 
 module.exports = {
-	
+    inicio: function (req, res) {
+
+        var queryEgresado = "SELECT * FROM egresado";
+        var queryEmpresa = "SELECT * FROM empresa";
+
+        Job.query(queryEgresado, [], function (err, resp) {
+            if (err) { return res.serverError(err); }
+
+            var egresados = [];
+
+            for (var x = 0; x < resp.length; x++) {
+                var egresado = {
+                    'id_egresado': resp[x].id_egresado,
+                    'nombreRut': resp[x].nombre + ' ' + resp[x].apellido + ' - ' + resp[x].rut
+                }
+                egresados.push(egresado);
+            }
+
+            egresadosGlobal = resp;
+
+
+            Job.query(queryEmpresa, [], function (err, resp) {
+                if (err) { return res.serverError(err); }
+
+                var empresas = [];
+
+                var empresa = {
+                    'id_empresa': "IDIDID",
+                    'nombreRutEspec': "NOMBRESILLO"
+                }
+                empresas.push(empresa);
+
+                return res.view('inicioEmpleos', {
+                    egresados: egresados,
+                    empresas: resp
+                });
+            });
+
+        });
+
+    },
+
+    getEgresadoSelect: function (req, res) {
+
+        var id = req.body.id_egresado;
+
+        var respuesta = [];
+
+        for (var x = 0; x < egresadosGlobal.length; x++) {
+            if (egresadosGlobal[x].id_egresado == id) {
+                respuesta.push(egresadosGlobal[x]);
+            }
+        }
+
+        return res.ok(respuesta);
+
+
+    },
+
+    getEgresados: function (req, res) {
+
+        var arg = req.body;
+        var query = "SELECT * FROM egresado where rut ='" + arg.rut + "'";
+
+        Job.query(query, [], function (err, resp) {
+            if (err) { return res.serverError(err); }
+
+            return res.ok(resp);
+
+        });
+    },
+
+    getEmpresas: function (req, res) {
+
+        Job.query('SELECT * FROM empresa', [], function (err, resp) {
+            if (err) { return res.serverError(err); }
+
+            return res.ok(resp);
+
+        });
+    },
+
+    getSupervisores: function (req, res) {
+
+        Job.query('SELECT * FROM supervisor', [], function (err, resp) {
+            if (err) { return res.serverError(err); }
+
+            return res.ok();
+
+        });
+    },
+
+    getEmpleos: function (req, res) {
+
+        Job.query('SELECT * FROM egresado as e inner join empleo as em on e.id_egresado = em.id_egresado', [], function (err, resp) {
+            if (err) { return res.serverError(err); }
+
+            return res.ok();
+
+        });
+    },
+
+    addEmpleo: function (req, res) {
+
+        var param = req.body;
+
+        if (param.fechaTermino != null) {
+            var fechasTermino = param.fechaTermino.split("/");
+            var fechaTermino = fechasTermino[2].toString() + '-' + fechasTermino[1].toString() + '-' + fechasTermino[0].toString();
+        }
+        else fechaTermino = null;
+
+
+        var fechasInicio = param.fechaInicio.split("/");
+        var fechaInicio = fechasInicio[2].toString() + '-' + fechasInicio[1].toString() + '-' + fechasInicio[0].toString();
+
+        var query = "INSERT INTO empleo VALUES(null,'" + param.egresadoSelect + "', ' " + param.empresa + "' ,null,null,null,'" + fechaInicio + "', '" + fechaTermino + "' ,'" + param.pais + "','" + param.comuna + "','" + param.cargo + "','" + param.sueldo + "')";
+
+        Job.query(query, [], function (err, resp) {
+            if (err) { return res.serverError(err); }
+
+            return res.ok(resp);
+        });
+        //return res.ok();
+    },
+
+    addFormSuperv: function (req, res) {
+
+        var param = req.body;
+        var query = "INSERT INTO `supervisor` VALUES (null,'" + param.rutSup + "' ,'" + param.nombreSup + "','" + param.correo + "','" + param.telefono + "','" + param.nota + "')";
+
+        Job.query(query, [], function (err, resp) {
+            if (err) { return res.serverError(err); }
+
+            return res.ok(resp);
+        });
+
+        //res.send("ok");
+    }
 };
 
--- config/routes.js
+++ config/routes.js
@@ -1,53 +1,32 @@
-/**
- * Route Mappings
- * (sails.config.routes)
- *
- * Your routes map URLs to views and controllers.
- *
- * If Sails receives a URL that doesn't match any of the routes below,
- * it will check for matching files (images, scripts, stylesheets, etc.)
- * in your assets directory.  e.g. `http://localhost:1337/images/foo.jpg`
-<<<<<<< HEAD
- * might match xan image file: `/assets/images/foo.jpg`
-=======
- * might match an image file: `/assets/images/foo.jpg`
->>>>>>> 4e51c91337a697686f26975949c84d0e43ffcf36
- *
- * Finally, if those don't match either, the default 404 handler is triggered.
- * See `api/responses/notFound.js` to adjust your app's 404 logic.
- *
- * Note: Sails doesn't ACTUALLY serve stuff from `assets`-- the default Gruntfile in Sails copies
- * flat files from `assets` to `.tmp/public`.  This allows you to do things like compile LESS or
- * CoffeeScript for the front-end.
- *
- * For more information on configuring custom routes, check out:
- * http://sailsjs.org/#!/documentation/concepts/Routes/RouteTargetSyntax.html
- */
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-  * etc. depending on your default view engine) your home page.              *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
-
-  '/':{
+  '/': {
     view: 'homepage'
   },
-  '/Empresa':'CompanyController.mostrar',
+  '/Empresa': 'CompanyController.mostrar',
   'post /Agregar': 'CompanyController.create',
-  'get /Eliminar/:id':'CompanyController.delete',
-  'get /Editar/:id':'CompanyController.edit',
-  'get /Ver/:id':'CompanyController.view',
-  'post /Actualizar':'CompanyController.update',
+  'get /Eliminar/:id': 'CompanyController.delete',
+  'get /Editar/:id': 'CompanyController.edit',
+  'get /Ver/:id': 'CompanyController.view',
+  'post /Actualizar': 'CompanyController.update',
 
-  '/': {
-    view: 'homepage',
+  '/getSupervisores': 'JobController.getSupervisores',
+  '/getEmpleos': 'JobController.getEmpleos',
+  'post /getEgresados': 'JobController.getEgresados',
+  'post /getEgresadoSelect': 'JobController.getEgresadoSelect',
+  '/getEmpresas': 'JobController.getEmpresas',
+
+  'post /addEmpleo': 'JobController.addEmpleo',
+
+  '/addFormSuperv': 'JobController.addFormSuperv',
+
+
+  '/inicioEmpleos':
+  'JobController.inicio',
+
+  '/addSupervisor': {
+    view: 'addSupervisor'
   },
 
   '/addEmpleo': {
@@ -61,14 +40,5 @@ module.exports.routes = {
     view: 'egresados'
   }
 
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the custom routes above, it   *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
 
 };
