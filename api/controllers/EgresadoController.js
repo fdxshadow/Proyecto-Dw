b67ef0a76f7c26c93fce910c4f07fe11adb375c4
CristianRojass <cristian.rojas@alumnos.uv.cl>
2017-11-14 17:39:00 -0300

    Modelo 'Egresado' añadido

---
+++ api/controllers/EgresadoController.js
@@ -0,0 +1,11 @@
+/**
+ * EgresadoController
+ *
+ * @description :: Server-side logic for managing egresadoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Egresado.js
@@ -0,0 +1,75 @@
+/**
+ * Egresado.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  
+  connection: 'someMysqlServer',
+  tableName: 'egresado',
+
+  attributes: {
+    id_egresado:{
+      type:'integer',
+      autoIncrement: true,
+      primaryKey: true
+    },
+  	nombre:{
+  		type:'string',
+      required:true
+  	},
+  	apellido:{
+  		type:'string',
+      required:true
+  	},
+  	rut:{
+  		type: 'string',
+      required:true,
+      unique:true
+  	},
+    año_ingreso:{
+      type:'date',
+      required:true
+    },
+    año_egreso:{
+    	type:'date',
+      required:true
+    },
+    año_titulacion:{
+    	type:'date'
+    },
+    carrera:{
+    	type:'string',
+      enum: ['ejecucion', 'informatica', 'civil']
+      required:true
+    },
+    postgrado:{
+    	type:'string',
+      enum: ['magister', 'doctorado']
+    },
+    area_postgrado:{
+    	type:'string'
+    },
+    sat_carrera:{
+    	type:'string'
+    },
+    nota_carrera:{
+    	type:'float'
+    },
+    linkedin:{
+    	type:'string'
+    },
+    nec_cap:{
+    	type:'string'
+    },
+    Cv:{
+      type:'string'
+    }
+  }
+
+};
