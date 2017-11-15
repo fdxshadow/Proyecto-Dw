3e509d1e1e3e0dab7b326cd744ece3ffac873c21
ivetteleonfaune <ivette.leon@alumnos.uv.cl>
2017-11-15 12:15:09 -0200

    Update and rename config/connections.js to  config/connections.js

--- config/connections.js
+++ config/connections.js
@@ -9,9 +9,3 @@ module.exports.connections = {
     password: '', //optional
     database: 'egresados' //optional
   },
-<<<<<<< HEAD
-};
-=======
-
-};
->>>>>>> 520dda55fe2beb00c65c7f456ffa41823f5cb8d7
