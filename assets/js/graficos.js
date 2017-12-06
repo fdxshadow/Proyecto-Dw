4e0cbea32f286282c611b1c724ab1e3587e03e21
daniel t <daniel_tr.93@hotmail.com>
2017-12-06 20:31:49 -0300

    grafico ajustado (front)

---
+++ assets/js/graficos.js
@@ -0,0 +1,22 @@
+var ctx = document.getElementById("grafico1");
+console.log(ctx);
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: ["Con empleo", "Sin empleo"],
+      datasets: [{
+        label: "Population (millions)",
+        backgroundColor: ["#3e95cd", "#8e5ea2"],
+        data: [10,30]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Comparacion de egresados con empleos'
+      }
+    }
+});
+
+ 
+// usar esa query select count(*) from egresado where id_egresado = any (select id_egresado from empleo);
