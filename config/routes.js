259ea97d3bcf5a18bbd5c31313fb4d501f25d011
Claudio Araya <claudio@idra.cl>
2017-12-06 19:58:42 -0300

    Mockup vista reporte1 y 5

--- config/routes.js
+++ config/routes.js
@@ -39,7 +39,7 @@ module.exports.routes = {
 
   '/graficoEmpEgr': { view:'graficoEmpleoEgresado' },
   '/reporte1':'ReportesController.reporte1',
-
+  '/reporte5':'ReportesController.reporte5',
 
 //MODULO GRÁFICO
 
